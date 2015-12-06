4a6b98a59087772b1b385998b958bb5a65d41da7
Richard Hull <rm_hull@yahoo.co.uk>
2015-12-06 02:05:47 +0000

    more poc

--- src/app/components/pages/components/app-bar.jsx
+++ src/app/components/pages/components/app-bar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import AppBar from 'material-ui/lib/app-bar';
 import Paper from 'material-ui/lib/paper';
+import AutoComplete from 'material-ui/lib/auto-complete';
+import RaisedButton from 'material-ui/lib/raised-button';
 
 const styles = {
   title: {
@@ -10,19 +11,19 @@ const styles = {
 
 export default class AppBarPage extends React.Component {
 
-  constructor(props) {
-    super(props);
-
-    this.desc = 'App bars are a collection of components placed as a static ' +
-                'header for an application. It is used for navigation, search ' +
-                'branding, and actions. An app bar is also referred to as the ' +
-                'primary toolbar or action bar for Android.';
-
-  }
-
   render() {
     return (
-      <div>{this.desc}</div>
+      <Paper>
+        <div>
+          <p>Search for movies by entering some keywords</p>
+
+          <AutoComplete
+              floatingLabelText="Movie keywords"
+              dataSource={["The Grand Budapest Hotel", "Jean de Florette", "Interstellar"]} />
+
+          <RaisedButton label="Search"/>
+        </div>
+      </Paper>
     );
   }
 
