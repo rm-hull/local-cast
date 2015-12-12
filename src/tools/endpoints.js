83e8e363dc80084ad7cbdc651007428dbc10265d
Richard Hull <rm_hull@yahoo.co.uk>
2015-12-12 00:31:33 +0000

    Rework mock search assembly

--- src/tools/endpoints.js
+++ src/tools/endpoints.js
@@ -20,7 +20,16 @@ function endpoints(request, body) {
       response: () =>
         error
           ? {"error" : "Something went wrong"}
-          : require(`./data/search${choice}.json`)
+          : [
+            require('./data/02d3d879e4b97527ce1f50e1f9609e935faf36aad2f4cae4bf0b233866682a11.json'),
+            require('./data/46fbffbaec5d98a0b3811ef3881478c4befb06d32b4928d5296e13cf464b1c6b.json'),
+            require('./data/5ee2f9c4c6f63dbfd440870dfc44c0055cb0c191907b712beb27d363c2b9e8b7.json'),
+            require('./data/6aa3fa37b0ecc0b34283eda6036e8ea1498c95abd14a403e3b8e0c74f6a52999.json'),
+            require('./data/8f9488a034665f83ea392e59d1161784da828a913ece3c3e52731a5f15bdf45c.json'),
+            require('./data/ccdafe97c4c078455fc0f28b2b22addb5ebf37de52d4d4197fc1990500d00580.json'),
+            require('./data/0978c6f9887896e58985a4a63eb8b853222729bb803785d080c1ad1fc59d788d.json'),
+            require('./data/b8bd0bec597adf7212ecdb4c064bb9ddf6c8546e7f003ed09075f8e81d0e01bd.json')
+          ]
     }
   ];
 }
